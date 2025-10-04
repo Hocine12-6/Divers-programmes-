c7a7468fdb27703fde9777a94d5def97f84632eb
Hocine Yacia <hocineff2@gmail.com>
2025-10-04 14:25:58 +0100

    Update Hang_man_AR.py

--- Hang_man_AR.py
+++ Hang_man_AR.py
@@ -1,4 +1,4 @@
-import random #استرحاع نموذج عشوتئية
+import random #استرجاع نموذج عشوتئية
 import string
 words = ['ant', 'baboon', 'badger', 'bat', 'bear', 'beaver', 'camel', 'cat', 'clam', 'cobra', 'cougar',
          'coyote', 'crow', 'deer', 'dog', 'donkey', 'duck', 'eagle', 'ferret', 'fox', 'frog', 'goat',
